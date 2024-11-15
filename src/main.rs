c735b91b1f88d1b8475891ebe283449984cd9b14
jannfz <jannfz@gmail.com>
2024-11-15 14:26:07 +0100

    added comments

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,30 @@ const MEMORY_CAPACITY: usize = 4096;
 
 fn main() -> io::Result<()> {
 
+    // cunstruct the Cpu struct
     let mut cpu = Cpu::new(MEMORY_CAPACITY);
     cpu.init();
 
+    // load binary file and handle error
     match cpu.load_file("ex2.bin") {
         Ok(i) => println!("{} bytes were loaded into memory", i),
         Err(e) => eprintln!("Error: {}", e),
     }
 
+    // print the content of the memory
     cpu.print_memory();
 
+    // tick loop - loops until Cpu.halt is true
     while !cpu.halt() {
+
+        // executes one tick
         match cpu.tick() {
             Ok(()) => continue,
             Err(e) => eprintln!("{}",e),
         }
     }
 
+    // print the content of registers and memory
     cpu.print_status();
 
     Ok(())
