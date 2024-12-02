f3e188ee35edcb913a903a88c6417866edd4022d
jannfz <jannfz@gmail.com>
2024-12-02 13:19:50 +0100

    move ex bin to test

---
+++ tests/ex2.s
@@ -0,0 +1,17 @@
+# my_program.s
+.section .text
+.globl main
+
+main:
+        addi    s0, zero, 64
+        addi    s1, zero, 256
+        addi    sp, sp, -16
+        sw      s0, 12(sp)
+        sw      s1, 8(sp)
+        mv      t6, zero
+        add     t3, a2, a1
+        slli    a6, a1, 2
+        lui     a3, 16
+        addi    a7, a3, -256
+        lui     t0, 4080
+        ecall
