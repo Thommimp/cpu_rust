4389b6cb5b5d657c9b6cf00ea6e75f92fd953ba6
jannfz <jannfz@gmail.com>
2024-12-02 15:13:16 +0100

    imp .res test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io;
 
 mod cpu;
@@ -11,15 +12,29 @@ const MEMORY_CAPACITY: usize = 1000 * 1024;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    let file_path = &args[1];
+    let bin_file_path = &args[1];
+    let res_file_path;
+    let res_test;
 
-    println!("File path: {}", file_path);
+    println!("Bin file path: {}", bin_file_path);
+    if args.len() == 3 {
+        res_test = true;
+    } else {
+        res_test = false;
+    };
+
+    println!("Arg {}", args.len());
 
     // cunstruct the Cpu struct
     let mut cpu = Cpu::new(MEMORY_CAPACITY);
 
     // load binary file and handle error
-    match cpu.load_file(file_path) {
+    match cpu.load_file(bin_file_path) {
+        Ok(i) => println!("{} bytes were loaded into memory", i),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+
+    match cpu.load_file(bin_file_path) {
         Ok(i) => println!("{} bytes were loaded into memory", i),
         Err(e) => eprintln!("Error: {}", e),
     }
@@ -40,5 +55,33 @@ fn main() -> io::Result<()> {
     cpu.print_status();
     cpu.print_memory();
 
+    if res_test {
+        let reg_dump = cpu.dump_registers();
+        res_file_path = &args[2];
+        let res = read_res_bin(res_file_path);
+
+        if reg_dump == res.unwrap() {
+            println!("OK – all register values match the res binary")
+        } else {
+            println!("ERR – some register values do not match the res binary")
+        }
+    }
+
     Ok(())
+}
+
+fn read_res_bin(path: &str) -> Result<[u32; 32], String> {
+    let file = fs::read(path).map_err(|e| format!("Failed to read file: {}", e))?;
+
+    if file.len() != 32 * 4 {
+        return Err(String::from("File is not exacly 128 bytes"));
+    }
+
+    let mut res: [u32; 32] = [0; 32];
+
+    for (i, chunk) in file.chunks_exact(4).enumerate() {
+        res[i] = u32::from_le_bytes(chunk.try_into().unwrap());
+    }
+
+    Ok(res)
 }
