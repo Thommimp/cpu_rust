06a73da6900097a25b6c1a09a4a629ee6ce04043
jannfz <jannfz@gmail.com>
2024-12-02 13:18:42 +0100

    imp file path arg

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,19 @@ mod register_file;
 
 use cpu::Cpu;
 
-const MEMORY_CAPACITY: usize = 1000*1024;
+const MEMORY_CAPACITY: usize = 1000 * 1024;
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let file_path = &args[1];
+
+    println!("File path: {}", file_path);
+
     // cunstruct the Cpu struct
     let mut cpu = Cpu::new(MEMORY_CAPACITY);
 
     // load binary file and handle error
-    match cpu.load_file("ex2.bin") {
+    match cpu.load_file(file_path) {
         Ok(i) => println!("{} bytes were loaded into memory", i),
         Err(e) => eprintln!("Error: {}", e),
     }
